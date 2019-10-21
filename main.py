e615eb1a8828f60398ac751c96793f057a2920a0
mwilson18 <56611893+mwilson18@users.noreply.github.com>
2019-10-21 16:57:46 -0400

    feature level fusion added

--- main.py
+++ main.py
@@ -1,37 +1,36 @@
 import features_lbp
-import features_pca1
-import features_landmarks
+import features_pca
 import matcher_nb
 import matcher_knn
 import performance
-import get_images
+import get_data
+import RF
+import numpy as np
 
-features = [1,2,3]
-matchers = [1,2]
-#data = load_images()
+feature = 3 #1 = PCA, 2 = LBP, 3 = Fusion
+matcher = 3 #1 = NB, 2 = KNN, 3 = RF
 
 ''' Load the data and their labels '''
-image_directory = '/Users/meghna/Desktop/Project 1/Project Data'
-X, y = get_images.get_images(image_directory)
+image_directory = r"C:\Users\Mary\Desktop\test"
+landmark_directory = r"C:\Users\Mary\Desktop\test"
+X, y = get_data.get_images(image_directory)
 
-for feature in features:
-    if feature == 1:
-        pca_X = features_pca1.pca(X)
-    if feature == 2:
-        lndmrk_X, lndmrk_y = features_landmarks(X)
-    if feature == 3:
-        lbp_X, lbp_y = features_lbp(X)
-
-#feature-level fusion
-for i,j in range(0,len(pca_X)):
-    X[i],y[i]=avg(pca_X[i],lndmrk_X[i],lbp_X[i]),avg(pca_y[i],lndmrk_y[i],lbp_y[i])
+if feature == 1:
+    X = features_pca.pca(X)
+elif feature == 2:
+    X = features_lbp.featuresLBP(X)
+elif feature == 3:
+    W = Z = X
+    W = features_pca.pca(W)
+    Z = features_lbp.featuresLBP(Z)
+    X = np.hstack((W, Z))
     
-    for matcher in matchers:
-        if matcher == 1:
-            genuine_scores, impostor_scores = matcher_nb(X, y)
-        if matcher == 2:
-            genuine_scores, impostor_scores = matcher_knn(X, y)
-            
-#score-level fusion
-        
-        performance(genuine_scores, impostor_scores)
+if matcher == 1:
+    gen_scores, imp_scores = matcher_nb.m_Np(X, y)
+elif matcher == 2:
+    gen_scores, imp_scores = matcher_knn.knn(X, y)
+elif matcher == 3:
+    gen_scores, imp_scores = RF.RF(X, y)
+    
+''' Performance assessment '''
+performance.perf(gen_scores, imp_scores)
